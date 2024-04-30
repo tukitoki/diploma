a8d47a193fbe475bee5e278f93a417e4102f36a4
r.pavel <tokichiihere@gmail.com>
2024-04-30 18:23:33 +0300

    forger changes

--- kt/settings.gradle.kts
+++ kt/settings.gradle.kts
@@ -5,3 +5,23 @@ include("discovery-service")
 include("gateway-service")
 include("shared")
 include("order-service")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        google()
+    }
+}
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
