9a04dfb4ca51340987860facb9f7c08e551d1286
r.pavel <tokichiihere@gmail.com>
2024-06-08 01:54:11 +0300

    SHARED: little upd table class

--- kt/shared/src/main/kotlin/ru/vsu/cs/raspopov/exposed/TimeAtLongIdTable.kt
+++ kt/shared/src/main/kotlin/ru/vsu/cs/raspopov/exposed/TimeAtLongIdTable.kt
@@ -4,7 +4,9 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
 
-abstract class TimeAtLongIdTable : LongIdTable() {
+abstract class TimeAtLongIdTable(
+    name: String = "",
+) : LongIdTable(name) {
 
     val createdAt = datetime("created_at").default(LocalDateTime.now())
     val updatedAt = datetime("updated_at").nullable()
